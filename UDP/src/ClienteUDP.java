bd0d523b12ebd68cd771eaf1bd70138368d6512a
Nicolas <n.rincond@uniandes.edu.co>
2020-04-04 02:52:46 -0500

    various fixes

--- UDP/src/ClienteUDP.java
+++ UDP/src/ClienteUDP.java
@@ -1,9 +1,13 @@
 import java.net.*;
+import java.nio.ByteBuffer;
 import java.util.Scanner;
 import java.io.*;
 public class ClienteUDP 
 {
 	private static LogCliente l;
+	
+	
+	
 	
 	public static void main(String[] args) {
 		  try {
@@ -11,9 +15,9 @@ public class ClienteUDP
 		   // 1. crear el socket por donde se enviara la peticion y se recibira
 		   // la respuesta..
 			   String archivo="250MiB.txt";
-			   String tamaño="250MB";
-			   int tamañoP=0;
-			   int tamañoPl=0;
+			   String tamano="250MB";
+			   int tamanoP=0;
+			   int tamanoPl=0;
 		   DatagramSocket socket = new DatagramSocket();
 
 		   // 2. crear datagrama para enviar la info. el datagrama contiene
@@ -23,57 +27,62 @@ public class ClienteUDP
 		   System.out.println("Id del cliente: "+codigo);
 		   String msj = "SendMeAFilePlease,cliente"+codigo; // msj a enviar.
 		   String ip = "127.0.0.1";
-		   int port = 45000;
+		   int port = 5555;
+		   
+		   byte[] bufferEnvio = new byte[1024];
+		   byte[] bufferRecept = new byte[1024];
 		   
 		   // Crear datagrama 1
-		   DatagramPacket paqueteEnvio = new DatagramPacket(msj.getBytes(),
-		     msj.length(), InetAddress.getByName(ip), port);
-		   	tamañoP=paqueteEnvio.getLength();
+		   DatagramPacket paqueteEnvio = new DatagramPacket(msj.getBytes(), msj.length(), InetAddress.getByName(ip), port);
+		   socket.send(paqueteEnvio); //Envio el mensaje para indicar que le envie el archivo
+		   tamanoP=paqueteEnvio.getLength();
 		   	
 		   // enviar paquete 1.
-		   socket.send(paqueteEnvio);
 		   //recibe paquete 2
-		   byte[] buffer = new byte[1024];
-		   DatagramPacket paqueteServidor = new DatagramPacket(buffer, 1024);
-		   socket.receive(paqueteServidor);
-		   String[] datos=new String(paqueteServidor.getData()).split(",");
-		   if(datos[0].equals("1"))
-			{
-				archivo="100MiB.txt";
-				tamaño="100MB";
-			}
-		   System.out.println("datos en el paq 2: "+ datos[0]+", "+datos[1]);
-			//Log iniatiates recibe Nombrearchivo y Tamaño
-		   l=new LogCliente(codigo, archivo, tamaño);
-		   
+		   DatagramPacket paqueteServidor = new DatagramPacket(bufferRecept, bufferRecept.length);
+		   socket.receive(paqueteServidor); //Recibe el numero de fragmentos
+		   ByteBuffer wrapped = ByteBuffer.wrap(bufferRecept);
+		   int numFragmentos = wrapped.getInt();
 
-		   // Recibir archivo...
-		   // Crear datagrama de recepcion.
-		   byte[] resp = new byte[100000000];
-		   DatagramPacket paqueteRecibido = new DatagramPacket(resp,
-		     resp.length);
+		   //Contar tiempo
 		   long TInicio, TFin, tiempo; 
-			TInicio = System.currentTimeMillis(); 
-		   // 3.2 recibir paquete.
-		   socket.receive(paqueteRecibido);
-		   //System.out.println(paqueteRecibido);
-		   tamañoPl=paqueteRecibido.getLength();
-		   System.out.println("numeroPaquetesResividos"+tamañoPl);
+		   TInicio = System.currentTimeMillis(); 
+		   //Recibir numero de fragmentos
+		   tamano = numFragmentos*1024/1000000 + "B";
+		   
+		   // Recibir archivo...
+		   for(int i = 0; i < numFragmentos; i++) {
+			   socket.receive(paqueteServidor);
+			   System.out.println(new String(paqueteServidor.getData()));
+		   }
+		   
+		   //Log iniatiates recibe Nombrearchivo y Tamaño
+		   l=new LogCliente(codigo, archivo, tamano);
+		   		   
+		   
+		   //Tiempo De transimision
+		   TFin = System.currentTimeMillis();
+		   tiempo = TFin - TInicio;
+		   
+		   //Envio Confirmacion TODO falta hash
+		   msj = "OK";
+		   paqueteEnvio = new DatagramPacket(msj.getBytes(), msj.length(), InetAddress.getByName(ip), port);
+		   socket.send(paqueteEnvio);
+		   
+		   tamanoPl=paqueteServidor.getLength();
+		   
+		   System.out.println("numeroPaquetesResividos"+tamanoPl);
 		   // 4. mostrar info...
-		   System.out.println("Server respondio desde "
-		     + paqueteRecibido.getAddress().getHostAddress()
-		     + " por el puerto " + paqueteRecibido.getPort());
-		   String archivo1=new String(paqueteRecibido.getData()); 
+		   
+		   //String archivo1=new String(paqueteServidor.getData()); 
 		   String nombre="Archivo"+codigo+".txt";
 		   String ruta = "./data/ArchivosEnviados/"+nombre;
            File file = new File(ruta);
            FileWriter fw = new FileWriter(file);
            BufferedWriter bw = new BufferedWriter(fw);
-           bw.write(archivo1);
+           //bw.write(archivo1);
            bw.close();
-           TFin = System.currentTimeMillis();
-           tiempo = TFin - TInicio;
-           l.agregarDatos(true, tiempo, tamañoPl, tamañoP, tamañoPl, tamañoP);
+           l.agregarDatos(true, tiempo, tamanoPl, tamanoP, tamanoPl, tamanoP);
 		   // 5. cerrar
 		   socket.close();
 
--- UDP/src/ServerUDP.java
+++ UDP/src/ServerUDP.java
@@ -1,4 +1,5 @@
 import java.net.*;
+import java.nio.ByteBuffer;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Date;
@@ -12,135 +13,67 @@ public class ServerUDP {
 	public static void main(String[] args) 
 	{
 		Scanner reader = new Scanner(System.in);
-		String numero = "";
+		int numArchivo = 0;
+		int numClientes = 0;
 		String archivo="250MiB.txt";
-		String tamaño="250MB";
-		int tamañoP=0;
-		int tamañoPl=0;
-
+		String tamArchivo="";
+		int tamPaquete=0;
+		int tamanoPl=0;
+		final String[] PATHS = {"./data/100MiB.txt", "./data/250MiB.txt"};
 		try 
 		{
+			
+			System.out.println("Numero de archivo(100->1;250->2):");
+			numArchivo = Integer.parseInt(reader.nextLine());
+			tamArchivo = numArchivo == 1 ? "100MB" : "250MB";
+			System.out.println("Numero de clientes:");
+			numClientes = Integer.parseInt(reader.nextLine());
+			
+			
 			System.out.println("server creado........");
 			// 1. crear el servidor..
-			DatagramSocket socket = new DatagramSocket(45000);
-
+			DatagramSocket socketServidor = new DatagramSocket(5555);
+			
 			// 2. recibir mensaje desde el cliente...
-
 			// 2.1 crear el paquete donde se recibe el mensaje.
-			byte[] buffer = new byte[1024];
-			DatagramPacket paqueteCliente = new DatagramPacket(buffer, 1024);
-			// 2.2 recibir el paquete. operacion bloqueante.
-			System.out.println("socket esperando....");
-			socket.receive(paqueteCliente);
-
-			//Leer paquete 1
-			String msj = new String(paqueteCliente.getData());
-			System.out.println("recibe de ip: "
-					+ paqueteCliente.getAddress().getHostAddress()
-					+ " desde el puerto " + paqueteCliente.getPort()
-					+ " este mensaje:" + new String(msj));
-
-
-			System.out.println("Numero de archivo(100->1;250->2):");
-			numero = reader.nextLine();
-			System.out.println("Numero de clientes:");
-			numero = numero+","+reader.nextLine()+",";
-			String[] datos=numero.split(",");
-			
-
-			//Client's Address and port 
-			InetAddress addr = paqueteCliente.getAddress();// la misma del
-			int port = paqueteCliente.getPort();
-			msj=numero;
-			DatagramPacket paqueteEnvio = new DatagramPacket(msj.getBytes(),
-					msj.length(), addr, port);
-			
-			// enviar paquete 2
-			socket.send(paqueteEnvio);
-			System.out.println("se envio paq 2.");
-
-
-			envio(datos[0],datos[1],addr,port, socket);
-			// el datagrama contiene la información del destino.
-
-
-
-
+			byte[] bufferEnvio = new byte[1024];
+			byte[] bufferRecept = new byte[1024];
+			while (true){
+				DatagramPacket paqueteServidor = new DatagramPacket(bufferRecept, bufferRecept.length);
+				// 2.2 recibir el paquete. operacion bloqueante.
+				System.out.println("socket esperando....");
+				socketServidor.receive(paqueteServidor);
+	
+				//Leer paquete 1
+				String msj = new String(paqueteServidor.getData());
+				System.out.println("recibe este mensaje:" + new String(msj));
+	
+				//Client's Address and port 
+				InetAddress addr = paqueteServidor.getAddress();// la misma del
+				int port = paqueteServidor.getPort();
+				File file = new File(PATHS[numArchivo-1]);
+				FileInputStream fis = new FileInputStream(PATHS[numArchivo-1]);
+				int count = (int) Math.ceil((double) file.length()/1024.0);
+				byte[] numfragmentos = ByteBuffer.allocate(4).putInt(count).array();
+				
+				DatagramPacket paqueteEnvio = new DatagramPacket(numfragmentos,numfragmentos.length, addr, port);
+				socketServidor.send(paqueteEnvio);
+				
+				while((count = fis.read(bufferRecept)) > 0) {
+					paqueteEnvio = new DatagramPacket(bufferRecept, bufferRecept.length, addr, port);
+					socketServidor.send(paqueteEnvio);
+				}
+				// enviar paquete 2
+				socketServidor.receive(paqueteServidor);
+				System.out.println("Confirmacion: " + new String(paqueteServidor.getData()));	
+	
+				fis.close();
+	
+			}
 			//4. cerrar el socket...
-			socket.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-	}
-	public static void envio(String fileSelection,String numClientes,InetAddress addr,int port,DatagramSocket socket) throws IOException
-	{
-		int valor1=Integer.parseInt(numClientes);
-		for(int x = 0; x < valor1;x++)
-		{
-			try {
-
-				if(valor1 < 10)
-				{
-					System.out.println("Numero de clientes menor al especificado.");
-				}
-				else if(Integer.parseInt(fileSelection) == 1)
-				{
-					FileInputStream fr1 = new FileInputStream("./data/100MiB.txt");
-					FileReader fr = new FileReader ("./data/100MiB.txt");
-					byte b[] = new byte[8192];
-					int valor=fr1.read(b, 0, b.length);
-					BufferedReader br = new BufferedReader(fr);
-					String v=br.readLine();
-					String linea="";
-					while(v!=null)
-					{
-						linea = linea+v;
-						v=br.readLine();
-					}
-					//System.out.println(linea);
-					fr.close();
-					fr1.close();  
-					DatagramPacket paqueteEnvio = new DatagramPacket(linea.getBytes(),
-							valor, addr, port);
-
-					// 3.2 enviar paquete...
-					socket.send(paqueteEnvio);
-					System.out.println("se envio");
-				}
-				else if(Integer.parseInt(fileSelection) == 2 )
-				{
-					//InputStream input = clients.get(x).getInputStream();  	                   
-
-					FileInputStream fr = new FileInputStream("./data/250MiB.txt");
-					FileReader fr1 = new FileReader ("./data/250MiB.txt");
-					byte b[] = new byte[8192];
-					int valor=fr.read(b, 0, b.length);
-					BufferedReader br = new BufferedReader(fr1);
-					String v=br.readLine();
-					String linea="";
-					while(v!=null)
-					{
-						linea = linea+v;
-						v=br.readLine();
-					}
-					fr.close();
-					fr1.close();  
-					DatagramPacket paqueteEnvio = new DatagramPacket(linea.getBytes(),
-							valor, addr, port);
-
-					// 3.2 enviar paquete...
-					socket.send(paqueteEnvio); 
-				}
-				else
-				{
-					System.out.println("Opcion incorrecta");
-				}
-
-			}
-			catch (IOException e) {
-			}
-
 		}
 	}
 }
